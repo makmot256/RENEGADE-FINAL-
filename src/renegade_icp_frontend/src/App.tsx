cd173d54e750f44abfc5e74c3ae9ba543a9e9c02
MAKMOT JOHNSON <makmot.kabirajohnson@students.mak.ac.ug>
2025-12-21 21:05:19 +0300

    browserRouter

--- src/renegade_icp_frontend/src/App.tsx
+++ src/renegade_icp_frontend/src/App.tsx
@@ -24,7 +24,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <BrowserRouter future={{ v7_startTransition: true }}>
           <Routes>
             <Route element={<Layout />}>
               <Route path="/" element={<HomePage />} />
