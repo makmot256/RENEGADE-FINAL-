c9679b212469e4de406ebf63f26525c417d1b3b7
MAKMOT JOHNSON <makmot.kabirajohnson@students.mak.ac.ug>
2025-12-28 12:32:42 +0300

    final touches

--- src/renegade_icp_frontend/src/components/Dashboard/SocialAccounts.tsx
+++ src/renegade_icp_frontend/src/components/Dashboard/SocialAccounts.tsx
@@ -166,17 +166,5 @@ const SocialAccounts: React.FC = () => {
 
 export default SocialAccounts;
 
-//  <Link2 className="mr-2 h-4 w-4" />
-//                 {account.connected ? "Disconnect" : "Connect"} Account
-//               </Button>
-//             </CardFooter>
-//           </Card>
-//         ))}
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default SocialAccounts;
 
 
