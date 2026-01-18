8b71cde39ffe5fb69b534b71b593f4f35fc94346
MAKMOT JOHNSON <makmot.kabirajohnson@students.mak.ac.ug>
2026-01-18 23:06:53 +0300

    renegade

--- src/renegade_icp_frontend/src/components/ui/accordion.tsx
+++ src/renegade_icp_frontend/src/components/ui/accordion.tsx
@@ -56,8 +56,6 @@ AccordionContent.displayName = AccordionPrimitive.Content.displayName
 export { Accordion, AccordionItem, AccordionTrigger, AccordionContent }
 
 
-
-
 // import * as React from "react"
 // import * as AccordionPrimitive from "@radix-ui/react-accordion"
 // import { ChevronDown } from "lucide-react"
@@ -97,21 +95,4 @@ export { Accordion, AccordionItem, AccordionTrigger, AccordionContent }
 //   </AccordionPrimitive.Header>
 // ))
 // AccordionTrigger.displayName = AccordionPrimitive.Trigger.displayName
-
-// const AccordionContent = React.forwardRef<
-//   React.ElementRef<typeof AccordionPrimitive.Content>,
-//   React.ComponentPropsWithoutRef<typeof AccordionPrimitive.Content>
-// >(({ className, children, ...props }, ref) => (
-//   <AccordionPrimitive.Content
-//     ref={ref}
-//     className="overflow-hidden text-sm transition-all data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down"
-//     {...props}
-//   >
-//     <div className={cn("pb-4 pt-0", className)}>{children}</div>
-//   </AccordionPrimitive.Content>
-// ))
-
-// AccordionContent.displayName = AccordionPrimitive.Content.displayName
-
-// export { Accordion, AccordionItem, AccordionTrigger, AccordionContent }
 
